ed8feafd32b89a7fc94a1e1a4754c834a2bfb447
LukeG-H <lukegranthamhall@gmail.com>
2025-12-19 16:32:03 +0000

    Removing duplicated comment

--- roman_to_integer/solution.py
+++ roman_to_integer/solution.py
@@ -47,7 +47,7 @@ class Solution:
             print(f"+ {value} * {multiplier}")
             result += value * multiplier
             print(f"Running total = {result}")
-        
+
         print(f"{s} = {result}")
         return result
 
@@ -65,8 +65,3 @@ if __name__ == '__main__':
     ans3 = Solution().romanToInt(tests[3]["s"])
     ans4 = Solution().romanToInt(tests[4]["s"])
 
-"""
-    I can be placed before V (5) and X (10) to make 4 and 9. 
-    X can be placed before L (50) and C (100) to make 40 and 90. 
-    C can be placed before D (500) and M (1000) to make 400 and 900.
-"""
