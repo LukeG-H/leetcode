bdac3af915ae1ead594096c6a29e7dc078511310
LukeG-H <lukegranthamhall@gmail.com>
2025-12-21 20:51:19 +0000

    Solved '169. Majority Element'(fixed bug)

--- 0169_majority_element/solution.py
+++ 0169_majority_element/solution.py
@@ -7,7 +7,11 @@ class Solution:
             if num not in counts.keys():
                 counts[num] = 0
             counts[num] += 1
-        return max(counts)
+
+            if counts[num] > (len(nums) / 2):
+                return num
+
+        return -1 
 
     def test_results(self, expected: int, actual: int) -> None:
         print(f"Expected: {expected}, Actual: {actual}")
@@ -18,9 +22,12 @@ if __name__ == '__main__':
     tests = {
         1: {"nums": [3,2,3], "expected": 3},
         2: {"nums": [2,2,1,1,1,2,2], "expected": 2},
+        3: {"nums": [3,3,4], "expected": 3},
     }
 
     ans1 = Solution().majorityElement(tests[1]["nums"])
     ans2 = Solution().majorityElement(tests[2]["nums"])
+    ans3 = Solution().majorityElement(tests[3]["nums"])
     Solution().test_results(tests[1]["expected"], actual=ans1)
     Solution().test_results(tests[2]["expected"], actual=ans2)
+    Solution().test_results(tests[3]["expected"], actual=ans3)
